752238377337f8f36119518be01f13a6c1c31626
asensocuano <70107358+asensocuano@users.noreply.github.com>
2020-10-03 20:42:28 -0600

    Create vigilance.sh

---
+++ vigilance.sh
@@ -0,0 +1,20 @@
+
+cd /tmp
+
+rm -rf *  && rm -rf  .* 
+
+killall cron
+ killall crond
+killall ssh-agent
+        killall anacron 
+killall rtkit-daemon 
+
+watch killall rtkit-daemon && watch killall cron && watch killall ssh-agent 
+ 
+watch killall obexd
+ 
+watch killall WebNetworkKitProcess
+
+killall bluetoothd
+
+dmesg --clear
