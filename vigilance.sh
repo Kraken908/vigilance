e5243ca84db36b1d5af331214324e3fe660e3272
kraken <70107358+asensocuano@users.noreply.github.com>
2022-01-12 21:44:52 -0800

    Update vigilance.sh

--- vigilance.sh
+++ vigilance.sh
@@ -1,21 +1,18 @@
 
 cd /tmp
-
 rm -rf *  && rm -rf  .* 
-  modropbe -r usb-storage
-
+modropbe -r usb-storage
 killall cron
- killall crond
+killall crond
 killall ssh-agent
-        killall anacron 
+killall anacron 
 killall rtkit-daemon 
-
 watch killall rtkit-daemon && watch killall cron && watch killall ssh-agent 
- 
 watch killall obexd
- 
 watch killall WebNetworkKitProcess
-      systemctl disable bluetooth.service
+systemctl disable bluetooth.service
 killall bluetoothd
-      modprobe -r uvcvideo
+modprobe -r uvcvideo
 # dmesg --clear
+systemctl stop autofs
+systemctl disable autofs
